7fc8e87f8e55fdd763c220e9a1531c4043a10a63
Jaime Hidalgo Fernández <jaimehidalgofernandez@gmail.com>
2021-10-14 16:20:29 +0200

    Hilos Primos Lambda

---
+++ Ej07_HilosPrimosLambda/src/Hilo.java
@@ -0,0 +1,32 @@
+public class Hilo extends Thread {
+    private int num;
+
+    public Hilo(int num){
+        this.num=num;
+    }
+
+     Runnable primo =  ()-> {
+
+        boolean primo=true;
+        System.out.println("Comienza el hilo "+Thread.currentThread().getName());
+        long inicio=System.currentTimeMillis();
+        if(num<=2){
+            primo = false;
+        }else{
+            for (int i=2; i<num; i++) {
+                if (num % i == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+        }
+
+        long fin =System.currentTimeMillis();
+        double tiempo = (double) ((fin - inicio));
+        if(primo){
+            System.out.println(num+ " es primo. Ha tardado en calcularlo "+tiempo+" milisegundos");
+        }else{
+            System.out.println(num+" no es primo. Ha tardado en calcularlo "+tiempo+" milisegundos");
+        }
+    };
+}
---
+++ Ej07_HilosPrimosLambda/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+        int num3;
+
+        System.out.println("Introduzca tres numeros y le diremos si son primo");
+
+        System.out.println("Introduzca el primer numero");
+        num1 = sc.nextInt();
+        Hilo h1 = new Hilo(num1);
+        Thread t1=new Thread(h1.primo);
+        t1.setName("Hilo 1");
+
+        System.out.println("Introduzca el primer segundo");
+        num2 = sc.nextInt();
+        Hilo h2 = new Hilo(num2);
+        Thread t2=new Thread(h2.primo);
+        t2.setName("Hilo 2");
+
+        System.out.println("Introduzca el primer tercero");
+        num3 = sc.nextInt();
+        Hilo h3 = new Hilo(num3);
+        Thread t3=new Thread(h3.primo);
+        t3.setName("Hilo 3");
+
+
+
+
+
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
