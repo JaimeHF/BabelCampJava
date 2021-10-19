9b821c824ed6f63bf88007c14ffbfebd25b23735
Jaime Hidalgo Fernández <jaimehidalgofernandez@gmail.com>
2021-10-19 13:19:47 +0200

    Ordenador Spring

---
+++ Ej_10Spring/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import entity.Ordenador;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+         ApplicationContext context =new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        Ordenador o= context.getBean("Ordenador",Ordenador.class);
+        System.out.println(o);
+
+    }
+}
